4fabc119fc7acdbfb812dc7044e4a1c34d50d380
Lyn Larsen <c95683@gmail.com>
2026-09-23 18:49:30 -0600

    guh

--- djangobackend/urls.py
+++ djangobackend/urls.py
@@ -26,8 +26,8 @@ urlpatterns = [
         path('index.html', TemplateView.as_view(template_name='index/index.html'), name='home'),
         path('members', TemplateView.as_view(template_name='members/members.html'), name='members'),
         path('members/index.html', TemplateView.as_view(template_name='members/members.html'), name='members'),
-        path('calender', TemplateView.as_view(template_name='calender/calender.html'), name='calender'),
-        path('calender/index.html', TemplateView.as_view(template_name='calender/calender.html'), name='calender'),
+        path('calendar', TemplateView.as_view(template_name='calendar/calendar.html'), name='calendar'),
+        path('calendar/index.html', TemplateView.as_view(template_name='calendar/calendar.html'), name='calendar'),
 ]
 def custom404(request, exception=None):
     return page_not_found(request, exception, template_name='404/404.html')
