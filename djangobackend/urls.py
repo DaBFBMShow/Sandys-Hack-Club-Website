e36c5be12e0685689535a529a2db10517d39b2f5
Lyn Larsen <c95683@gmail.com>
2026-09-23 18:48:27 -0600

    added calender to urls

--- djangobackend/urls.py
+++ djangobackend/urls.py
@@ -26,6 +26,8 @@ urlpatterns = [
         path('index.html', TemplateView.as_view(template_name='index/index.html'), name='home'),
         path('members', TemplateView.as_view(template_name='members/members.html'), name='members'),
         path('members/index.html', TemplateView.as_view(template_name='members/members.html'), name='members'),
+        path('calender', TemplateView.as_view(template_name='calender/calender.html'), name='calender'),
+        path('calender/index.html', TemplateView.as_view(template_name='calender/calender.html'), name='calender'),
 ]
 def custom404(request, exception=None):
     return page_not_found(request, exception, template_name='404/404.html')
